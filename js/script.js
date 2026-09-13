8e99b744f595e618c62c74205e3925d33210c267
shinta amelia <ameliashita100@gmail.com>
2026-09-13 11:47:13 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const promoButton = document.queryselector("#promoButtton");
+const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
     promoButton.textContent = "Promo: Beli 2 Gratis Tester!";
